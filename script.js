02e90a0f6998eca58236aad92906054b2c93aa18
Graham Lambe <lambegraham@gmail.com>
2020-08-06 13:51:53 +0100

    Second Page Edits

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
     /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
+    $(window).scroll(function () {
+
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
+        $('.hideme').each(function (i) {
+
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
+
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 500);
+
             }
-            
-        }); 
-    
+        });
     });
-    
+    $('.fadein').fadeIn(3000);
 });
